a649551e78e179731d2800cbe6e784729b268245
Shaun McFall <shaunm.mcfall@gmail.com>
2020-03-01 13:43:37 -0500

    v1.1 update, and some text fixes, and it's a slider now

--- Source/Patches/FilthMaker_MakeFilth_Patch.cs
+++ Source/Patches/FilthMaker_MakeFilth_Patch.cs
@@ -1,15 +1,9 @@
-﻿using Harmony;
-using RimWorld;
-using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using HarmonyLib;
 using System.Linq;
-using System.Text;
 using Verse;
 
 namespace Merthsoft.NoDirt {
-    [HarmonyPatch(typeof(GenSpawn), "Spawn", 
-        new[] { typeof(Thing), typeof(IntVec3), typeof(Map), typeof(Rot4), typeof(WipeMode), typeof(bool) })]
+    [HarmonyPatch(typeof(GenSpawn), "Spawn", new[] { typeof(Thing), typeof(IntVec3), typeof(Map), typeof(Rot4), typeof(WipeMode), typeof(bool) })]
     class GenSpawn_Spawn_Patch {
         public static bool Prefix(ref Thing newThing, ref IntVec3 loc, ref Map map) {
             if (!newThing.def.IsFilth) { return true; }
---
+++ Source/TranslationKeys.cs
@@ -0,0 +1,11 @@
+﻿namespace Merthsoft.NoDirt {
+    public static class TranslationKeys {
+        public static string SettingsCategory => "merthsoft_nodirt_settings_category";
+
+        public static string Explanation => "merthsoft_nodirt_settings_explanation";
+        
+        public static string SettingsInHome => "merthsoft_nodirt_settings_in_home";
+        public static string SettingsOutsideHome => "merthsoft_nodirt_settings_outside_home";
+        public static string SettingsFilth => "merthsoft_nodirt_settings_filth_desc";
+    }
+}
